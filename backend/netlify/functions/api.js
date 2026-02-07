c2ddbb84c0dacb76cf484cc88e033bac00b33e22
brunoroquevtal <bruno.roque@vtal.com>
2026-02-07 01:33:11 -0300

    Feat: Configurar backend como git submodule
    - Backend agora aponta para o repositÃ³rio CRQExecComMgmtBackend
    - Atualizado .gitignore para nÃ£o ignorar backend (submodule)
    - Facilita sincronizaÃ§Ã£o entre repositÃ³rios
    - Backend serÃ¡ atualizado automaticamente quando o submodule for atualizado

--- backend/netlify/functions/api.js
+++
@@ -1,9 +0,0 @@
-/**
- * Netlify Function wrapper para o servidor Express
- * Permite que o backend Express funcione como Netlify Function
- */
-const serverless = require('serverless-http');
-const app = require('../../server.js');
-
-// Exportar handler para Netlify Functions
-module.exports.handler = serverless(app);
